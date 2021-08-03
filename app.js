eb6b32f4c81994eea8573b03989b95f0dd14d814
Barak Ben Gur <barakbbg@gmail.com>
2021-08-03 14:25:47 +0300

    first draft of search map

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const myTable = document.querySelector("#myTable");
 let titles = document.querySelector("tr#table-titles");
 let tableBody = document.querySelector("tbody#table-body");
 
+// map
+
 // build the table
 const drawTable = (content) => {
   // console.log(content);
@@ -54,44 +56,57 @@ const drawTitles = (allTitles) => {
 
 //----------------FETCH DATA----------------
 const response = axios({
-  url: "https://data.gov.il/api/3/action/datastore_search?resource_id=b9d690de-0a9c-45ef-9ced-3e5957776b26&limit=200",
+  url: "https://data.gov.il/api/3/action/datastore_search?resource_id=b9d690de-0a9c-45ef-9ced-3e5957776b26&limit=500",
   method: "GET",
 }).then((res) => {
   drawTable(res.data.result);
   const dataTable = new DataTable(myTable);
+  showOnMap(res.data.result.records);
 });
 
-//----------------MAP-----------------------------
+//----------------MAP--------------------------
+
+const showOnMap = (dataAtm) => {
+  const dataTableInput = document.querySelector(".dataTable-input");
+  dataTableInput.addEventListener("input", (event) => {
+   const searchValue = event.target.value;
+    const data = dataAtm
+    // console.log(dataTable.searchData);
+    // console.log(dataTable.data()[0]);
+
+    let filtered = filterTable(data, searchValue)
+    console.log(filtered);
+    let filteredFields = getCoords(filtered)
+    // selectorMapElement.innerHTML = ''
+    // map = new google.maps.Map(selectorMapElement, myOptions)
+    // init_map(filteredFields)
+  });
+};
+
+// filter results
+const filterTable = (data, val) => {
+  return data.filter(atm => {
+    return atm.City.includes(val)
+  })
+}
 
 const getCoords = (coords) => {
-  console.log(coords);
+  // console.log(coords);
   const coordsArr = [];
   for (let index = 0; index < coords.length; index++) {
     let coordsEl = {
       x: coords[index].X_Coordinate,
       y: coords[index].Y_Coordinate,
       address: coords[index].ATM_Address,
-      name: coords[index].Bank_Name
+      name: coords[index].Bank_Name,
     };
     coordsArr.push(coordsEl);
   }
-  // console.log(coordsArr);
+
   init_map(coordsArr);
 };
 
-// const showOnMap = (coordsArr) => {
-//   // cosnt markerArr = []
-//   for (let index = 0; index < coordsArr.length; index++) {
-//        const marker = new google.maps.Marker({
-//         map: map,
-//         position: new google.maps.LatLng(coordsArr.x, coordsArr.y),
-//       });
-
-//   }
-// };
-
 function init_map(coordsArr) {
-
   // console.log("coordsArr are: ", coordsArr);
   let selectorMapElement = document.querySelector("#gmap_canvas");
   let googleMapTitle = "ATM 1";
@@ -104,13 +119,6 @@ function init_map(coordsArr) {
     center: new google.maps.LatLng(googleMapLat, googleMapLong),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
   };
-
-  // let infowindow = new google.maps.InfoWindow({
-  //   content: `
-  //       <strong>${googleMapTitle}</strong>
-  //       <br>${googleMapAddress}<br>
-  //     `
-  // });
 
   const map = new google.maps.Map(selectorMapElement, myOptions);
 
@@ -128,18 +136,9 @@ function init_map(coordsArr) {
       content: `
           <strong>${coordsArr[index].name}</strong>
           <br>${coordsArr[index].address}<br>
-        `
+        `,
     });
   } // for
-
-  // marker2 = new google.maps.Marker({
-  //   map: map,
-  //   position: new google.maps.LatLng(38.2847678, -122.9536827)
-  // });
-
-  //   google.maps.event.addListener(marker2, 'click', function() {
-  //     infowindow.open(map, marker2);
-  // });
 }
 
 google.maps.event.addDomListener(window, "load", init_map);
